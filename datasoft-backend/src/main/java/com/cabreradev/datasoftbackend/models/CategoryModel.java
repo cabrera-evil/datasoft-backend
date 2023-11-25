281a7d413620093a492af19e876db7241d5a4ec4
cabrera-evil <douglascabrera.dev@gmail.com>
2023-11-25 12:26:08 -0600

    Feat: find products by category/id

--- datasoft-backend/src/main/java/com/cabreradev/datasoftbackend/models/CategoryModel.java
+++ datasoft-backend/src/main/java/com/cabreradev/datasoftbackend/models/CategoryModel.java
@@ -7,30 +7,30 @@ import jakarta.persistence.*;
 public class CategoryModel {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private Long cat_id;
+    private Long catId;
     @Column(name = "cat_name", unique = true, nullable = false)
-    private String cat_name;
+    private String catName;
 
     public CategoryModel() {}
 
-    public CategoryModel(Long cat_id, String cat_name) {
-        this.cat_id = cat_id;
-        this.cat_name = cat_name;
+    public CategoryModel(Long catId, String catName) {
+        this.catId = catId;
+        this.catName = catName;
     }
 
-    public Long getCat_id() {
-        return cat_id;
+    public Long getCatId() {
+        return catId;
     }
 
-    public void setCat_id(Long cat_id) {
-        this.cat_id = cat_id;
+    public void setCatId(Long catId) {
+        this.catId = catId;
     }
 
-    public String getCat_name() {
-        return cat_name;
+    public String getCatName() {
+        return catName;
     }
 
-    public void setCat_name(String cat_name) {
-        this.cat_name = cat_name;
+    public void setCatName(String catName) {
+        this.catName = catName;
     }
 }
--- datasoft-backend/src/main/java/com/cabreradev/datasoftbackend/repositories/ICategoryRepository.java
+++ datasoft-backend/src/main/java/com/cabreradev/datasoftbackend/repositories/ICategoryRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICategoryRepository extends JpaRepository<CategoryModel, Long> {
-}
+public interface ICategoryRepository extends JpaRepository<CategoryModel, Long> { }
