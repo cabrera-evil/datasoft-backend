0d2cda265cae0e9bc441bd6eb87c2452810614ec
cabrera-evil <douglascabrera.dev@gmail.com>
2023-11-25 10:00:35 -0600

    Feat: base project with database connection

---
+++ datasoft-backend/src/main/java/com/cabreradev/datasoftbackend/DatasoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cabreradev.datasoftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasoftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasoftBackendApplication.class, args);
+    }
+
+}
---
+++ datasoft-backend/src/test/java/com/cabreradev/datasoftbackend/DatasoftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabreradev.datasoftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasoftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
